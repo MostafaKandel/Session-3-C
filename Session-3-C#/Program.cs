1c14f7a97b5901b28f41df2ea45573cdd744eb02
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-07-10 12:52:57 +0200

    feat: implementation of question 1

--- Session-3-C#/Program.cs
+++ Session-3-C#/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region question1
+            //Write a program that allows the user to enter a number then print it.
+            Console.WriteLine("Please enter a number:");
+            bool isValid= int.TryParse(Console.ReadLine(), out int number);
+            if (isValid)
+            {
+                Console.WriteLine($"The entered number: {number}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+            #endregion
         }
     }
 }
