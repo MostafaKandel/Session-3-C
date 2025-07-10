c872d6fea816b9f402d4cb69d3c282ebe31632eb
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-07-10 13:35:42 +0200

    feat: Q8

--- Session-3-C#/Program.cs
+++ Session-3-C#/Program.cs
@@ -4,54 +4,97 @@
     {
         static void Main(string[] args)
         {
-            //#region question1
-            ////Write a program that allows the user to enter a number then print it.
-            //Console.WriteLine("Please enter a number:");
-            //bool isValid = int.TryParse(Console.ReadLine(), out int number);
-            //if (isValid)
-            //{
-            //    Console.WriteLine($"The entered number: {number}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Invalid input. Please enter a valid number.");
-            //}
+            #region question1
+            //Write a program that allows the user to enter a number then print it.
+            /*
+            Console.WriteLine("Please enter a number:");
+            bool isValid = int.TryParse(Console.ReadLine(), out int number);
+            if (isValid)
+            {
+                Console.WriteLine($"The entered number: {number}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            */
 
-            //#endregion
-            //#region question2
-            ////2.	Write C# program that converts a string to an integer,
-            ////but the string contains non-numeric characters. And mention what will happen
+            #endregion
 
-            //string input = "Ahmed123";
-            //// by using convert
-            //int convertedNumber = Convert.ToInt32(input);
-            //Console.WriteLine($"Converted number: {convertedNumber}"); // in this case , it will throw an exception because the string is not in valid format (non-numeric characters).
+            #region question2
+            //2.	Write C# program that converts a string to an integer,
+            //but the string contains non-numeric characters. And mention what will happen
+            /*
 
-            //// by using int.Parse
-            //int parsedNumber= int.Parse(input); // in this case , it will throw an exception because the string is not in valid format (non-numeric characters) and also if the string is null.
-            //Console.WriteLine($"Parsed number: {parsedNumber}");
+            string input = "Ahmed123";
+            // by using convert
+            int convertedNumber = Convert.ToInt32(input);
+            Console.WriteLine($"Converted number: {convertedNumber}"); // in this case , it will throw an exception because the string is not in valid format (non-numeric characters).
 
-            //// by using TryParse
-            //int.TryParse(input, out int result);
-            //Console.WriteLine($"TryParse result: {result}"); // in this case, it will return 0 because the string is not in valid format (non-numeric characters) and it will not throw an exception.
+            // by using int.Parse
+            int parsedNumber = int.Parse(input); // in this case , it will throw an exception because the string is not in valid format (non-numeric characters) and also if the string is null.
+            Console.WriteLine($"Parsed number: {parsedNumber}");
 
-            //#endregion
+            // by using TryParse
+            int.TryParse(input, out int result);
+            Console.WriteLine($"TryParse result: {result}"); // in this case, it will return 0 because the string is not in valid format (non-numeric characters) and it will not throw an exception.
+            */
+            #endregion
 
 
-            //#region question3
-            ////Write C# program that Perform a simple arithmetic operation with floating-point numbers And mention what will happen
-            //#endregion
+            #region question3
+            //Write C# program that Perform a simple arithmetic operation with floating-point numbers And mention what will happen
+            #endregion
 
             #region question7
             //Write C# program that take two string variables and print them as one variable 
+            /*
             Console.WriteLine("Please enter the first string:");
             string firstString = Console.ReadLine() ?? " "; // to show " " in case of null value
             Console.WriteLine("Please enter the second string:");
             string secondString = Console.ReadLine() ?? " ";
             Console.WriteLine($"Combined string: {firstString} {secondString}");
+            */
+            #endregion
 
+            #region question8
+            //Write a program that calculates the simple interest given the principal amount, rate of interest, and time. 
+            //Note : The formula for simple interest is Interest = (principal * rate * time ) /100.
+
+            /*
+            Console.WriteLine("Please enter the principal amount:");
+            double.TryParse(Console.ReadLine(), out double principal);
+            Console.WriteLine("Please enter the rate of interest:");
+            double.TryParse(Console.ReadLine(), out double rate);
+            Console.WriteLine("Please enter the time in years:");
+            double.TryParse(Console.ReadLine(), out double time);
+            double interest = (principal * rate * time) / 100;
+            Console.WriteLine($"The simple interest is: {interest}");
+            */
+
+            // to be more safe 
+            /*
+            Console.WriteLine("Please enter the principal amount:");
+            bool isPrincipalValid= double.TryParse(Console.ReadLine(), out double principal);
+            Console.WriteLine("Please enter the rate of interest:");
+            bool isRateValid= double.TryParse(Console.ReadLine(), out double rate);
+            Console.WriteLine("Please enter the time in years:");
+            bool isTimeValid= double.TryParse(Console.ReadLine(), out double time);
+            if(isPrincipalValid && isRateValid && isTimeValid)
+            {
+                double interest = (principal * rate * time) / 100;
+                Console.WriteLine($"The simple interest is: {interest}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid inputs. Please enter valid numbers.");
+
+            }
+            */
 
             #endregion
+
+
 
         }
     }
